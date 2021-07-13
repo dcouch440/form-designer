53493477b0413d78851464638e5f7e531a494309
dcouch440 <dcouch440@gmail.com>
2021-07-12 17:43:22 -0700

    add form and app style options

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import AppStyleOptions from "./components/StyleOptionsMenu";
+import GlobalStyles from "./GlobalStyles";
+import Form from "./components/Form";
 
 export default function App () {
   return (
-    <></>
+    <>
+      <GlobalStyles />
+      <AppStyleOptions />
+      <Form />
+    </>
   );
 }
