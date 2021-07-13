304db949f5875795d5a2bf6126072e43901dbbd5
dcouch440 <dcouch440@gmail.com>
2021-07-12 23:07:08 -0700

    add component styles

---
+++ src/components/StyleOptionsCurrentColorsBlock/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ColorBlock = styled.div`
+  background-color: ${({backgroundColor}) => backgroundColor};
+  height: 25px;
+  border: 2px solid black;
+  box-shadow: 0 0 1px white;
+  position: relative;
+`;
+
+export const DropWindow = styled.div`
+  position: absolute;
+  z-index: 2;
+  background-color: black;
+  padding: 5px;
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+`;
