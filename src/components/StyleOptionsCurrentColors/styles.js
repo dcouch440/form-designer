3ac7dfe8599695d69739c73cadfdbb37952e0696
dcouch440 <dcouch440@gmail.com>
2021-07-12 23:07:21 -0700

    create component

---
+++ src/components/StyleOptionsCurrentColors/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ColorBlockContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  grid-gap: 5px;
+  margin-bottom: 5px;
+`;
