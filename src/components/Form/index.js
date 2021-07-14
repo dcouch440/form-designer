fa4066005c86205cef21603037ca2db1d460fc44
dcouch440 <dcouch440@gmail.com>
2021-07-14 14:09:49 -0700

    add drop

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,10 +1,39 @@
-import React from 'react'
-import AppButton from '../AppButton'
+import React, { useEffect, useState } from 'react'
+import { useDrop } from 'react-dnd';
+import createComponentObject from '../../functions/createComponentObject';
 
+import {
+  FormContainer
+} from './styles';
+// [createComponentObject({ type: 'button' })]
 export default function Form () {
+  const [inputs, setInputs] = useState([]);
+  const [{ item, dropped }, drop] = useDrop(() => ({
+    // The type (or types) to accept - strings or symbols
+    accept: 'BOX',
+    // Props to collect
+    collect: monitor => ({
+      item: monitor.getItem(),
+      dropped: monitor.didDrop()
+    })
+  }));
+
+  useEffect(() => {
+    if (dropped) {
+      setInputs(prev => [...prev, createComponentObject({ type: item.name })])
+    }
+  }, [dropped, item]);
+
+  console.log({dropped})
+
   return (
-    <AppButton
-      text='does the dog run'
-    />
+    <FormContainer
+      ref={drop}
+      role={'Dustbin'}
+    >
+      {
+        inputs.map(({ Render }, index) => <Render key={index}/>)
+      }
+    </FormContainer>
   )
 }
---
+++ src/components/Form/styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  flex: 10;
+  height: 100%;
+`;
