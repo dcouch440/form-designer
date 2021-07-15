73e23e4cf7cb8eacbbec5c498afdc49e5ab1a780
dcouch440 <dcouch440@gmail.com>
2021-07-14 19:37:19 -0700

    add modal

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useDrop } from 'react-dnd';
 import createComponentObject from '../../functions/createComponentObject';
+import { StylesContext } from '../../StylesContext';
+import FormModal from '../FormModal';
 
 import {
   FormContainer
 } from './styles';
 // [createComponentObject({ type: 'button' })]
 export default function Form () {
+  const { appStyles } = useContext(StylesContext);
   const [inputs, setInputs] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [currentItem, setCurrentItem] = useState('');
   const [{ item, dropped }, drop] = useDrop(() => ({
     // The type (or types) to accept - strings or symbols
     accept: 'BOX',
@@ -20,19 +25,35 @@ export default function Form () {
 
   useEffect(() => {
     if (dropped) {
-      setInputs(prev => [...prev, createComponentObject({ type: item.name })])
+      setModal(true);
+      setCurrentItem(item.name)
     }
   }, [dropped, item]);
 
-  console.log({dropped})
+  const handleSubmit = props => {
+    setInputs(prev => [...prev, createComponentObject({
+      type: currentItem,
+      props,
+    })]);
+    setModal(false);
+    setCurrentItem('')
+  }
 
   return (
     <FormContainer
+      backgroundColor={appStyles.page.nonHovered.backgroundColor}
       ref={drop}
       role={'Dustbin'}
     >
       {
-        inputs.map(({ Render }, index) => <Render key={index}/>)
+        inputs.map(({ Render, props }, key) => <Render title={props.title} key={key} />)
+      }
+      {
+        modal &&
+          <FormModal
+            type={currentItem}
+            onSubmit={handleSubmit}
+          />
       }
     </FormContainer>
   )
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
 export const FormContainer = styled.div`
+  position: relative;
   flex: 10;
-  height: 100%;
+  display: grid;
+  grid-auto-rows: min-content;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  > * {
+    padding: 7px;
+  }
 `;
