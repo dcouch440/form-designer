97712a53367e100b3dae0c3467728dadb19991e6
dcouch440 <dcouch440@gmail.com>
2021-07-14 19:36:31 -0700

    add drag provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { StylesContextProvider } from './StylesContext';
-
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 ReactDOM.render(
   <StylesContextProvider>
     <BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
       <App />
+    </DndProvider>
     </BrowserRouter>
   </StylesContextProvider>,
   document.getElementById('root')
