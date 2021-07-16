ccba8df78105bdc076de91dcdd3c82240f0d6207
dcouch440 <dcouch440@gmail.com>
2021-07-15 19:28:34 -0700

    fix indent

--- src/hooks/useHover/index.js
+++ src/hooks/useHover/index.js
@@ -1,6 +1,6 @@
-import {useEffect , useRef, useState} from 'react';
+import { useEffect , useRef, useState } from 'react';
 
-export default function useHover() {
+export default function useHover () {
   const [isHovered, setIsHovered] = useState(false);
   const ref = useRef(null);
   const handleMouseOver = () => setIsHovered(true);
@@ -10,12 +10,12 @@ export default function useHover() {
   useEffect(() => {
     const node = ref.current;
     if (node) {
-        node.addEventListener('mouseover', handleMouseOver);
-        node.addEventListener('mouseout', handleMouseOut);
-        return () => {
-          node.removeEventListener('mouseover', handleMouseOver);
-          node.removeEventListener('mouseout', handleMouseOut);
-        }
+      node.addEventListener('mouseover', handleMouseOver);
+      node.addEventListener('mouseout', handleMouseOut);
+      return () => {
+        node.removeEventListener('mouseover', handleMouseOver);
+        node.removeEventListener('mouseout', handleMouseOut);
+      };
     }
   }, [runIf]);
 
