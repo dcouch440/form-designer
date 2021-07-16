454f795dd6bb322ad1966f6b3a301aa09ed60e9f
dcouch440 <dcouch440@gmail.com>
2021-07-15 18:05:08 -0700

    change context name

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { StylesContextProvider } from './StylesContext';
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { DndProvider } from 'react-dnd'
+import { ContextProvider } from './Context';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 
 ReactDOM.render(
-  <StylesContextProvider>
+  <ContextProvider>
     <BrowserRouter>
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
     </BrowserRouter>
-  </StylesContextProvider>,
+  </ContextProvider>,
   document.getElementById('root')
 );
