aac50c980a8a3d718bb3fa02fc70bf67f4314315
dcouch440 <dcouch440@gmail.com>
2021-07-15 19:27:36 -0700

    add canSet

--- src/components/StyleOptionsButton/index.js
+++ src/components/StyleOptionsButton/index.js
@@ -1,28 +1,45 @@
-import React from 'react'
-
 import {
   ButtonGrid,
   SelectionButton
 } from './styles';
 
-export default function StyleOptionsButton ({ elementTypes, onClick, span, activeButton }) {
+export default function StyleOptionsButton ({
+  elementTypes,
+  onClick,
+  span,
+  activeButton,
+  slim,
+  columns
+}) {
+
+  const renderButtonsWithRules = () => {
+    const ifSet = ({ can, cant, canSet }) => canSet ? can : cant;
+
+    return (
+      elementTypes.map(({ set, buttonText, canSet }, index) => {
+        const isActive = set === activeButton;
+        const checkIfPageText = ifSet({ can: buttonText, cant: 'Cannot Set', canSet });
+
+        return (
+          <SelectionButton
+            key={index}
+            span={span}
+            isActive={isActive}
+            slim={slim}
+            onClick={() => onClick(set)}
+          >
+            { checkIfPageText }
+          </SelectionButton>
+        );
+      })
+    );
+  };
+
   return (
-    <ButtonGrid>
-      {
-        elementTypes.map(({ set, buttonText }, index) => {
-          const isActive = set === activeButton;
-          return (
-            <SelectionButton
-              key={index}
-              span={span}
-              isActive={isActive}
-              onClick={() => onClick(set)}
-            >
-              { buttonText }
-            </SelectionButton>
-          )
-        })
-      }
+    <ButtonGrid
+      columns={columns}
+    >
+      { renderButtonsWithRules() }
     </ButtonGrid>
-  )
+  );
 }
--- src/components/StyleOptionsButton/styles.js
+++ src/components/StyleOptionsButton/styles.js
@@ -4,24 +4,25 @@ export const ButtonGrid = styled.div`
   display: grid;
   grid-gap: '7px';
   margin-bottom: 10px;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: ${({ columns }) => columns ? `repeat(${columns}, 1fr)` : 'repeat(4, 1fr)'};
 `;
 
 export const SelectionButton = styled.button`
   transition: 0.2s;
-  height: 100px;
-  grid-column: ${({span}) => span ? 'span ' + span : null};
-  background-color: ${({isActive}) => isActive ? '#00bcd4' : '#9e9e9e'};
-  color: ${({isActive}) => isActive ? 'white' : 'black'};
   font-weight: 500;
-  font-size: ${({isActive}) => isActive ? '16px' : '15px'};
   font-family: sans-serif;
   border-radius: 7px;
   box-shadow: inset 0px 0px 4px 3px black;
   border: none;
+  height: ${({ slim }) => slim ? '50px' : '100px'};
+  font-size: ${({ isActive, slim }) => slim ? '12px' : isActive ? '16px' : '15px'};
+  color: ${({ isActive }) => isActive ? 'white' : 'black'};
+  background-color: ${({ isActive }) => isActive ? '#00bcd4' : '#9e9e9e'};
+  grid-column: ${({ span }) => span ? 'span ' + span : null};
   :hover {
     color: white;
     background-color: black;
     box-shadow: inset 0px 0px 2px white, 0px 0px 2px white;
   }
+
 `;
