f484b1cd3d1cd5dfdc0627c50d30072f7d528f8d
dcouch440 <dcouch440@gmail.com>
2021-07-15 19:28:02 -0700

    add key

--- src/components/AppButton/index.js
+++ src/components/AppButton/index.js
@@ -1,13 +1,14 @@
 import useAppStyles from '../../hooks/useAppStyles';
 
-export default function AppButton ({ onClick, text }) {
-  const { styles, ref } = useAppStyles({ element: 'button' })
+export default function AppButton ({ onClick, text, key }) {
+  const { styles, ref } = useAppStyles({ element: 'button' });
 
   return (
     <button
       ref={ref}
       style={styles}
       onClick={onClick}
+      key={key}
     >
       {text}
     </button>
