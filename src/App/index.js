c3efa8a7aa36b74c82fc08fe7b1e6be02a4869ab
dcouch440 <dcouch440@gmail.com>
2021-07-20 15:59:34 -0700

    add VeiwForm

--- src/App/index.js
+++ src/App/index.js
@@ -2,14 +2,23 @@ import StyleOptionsMenu from "../components/StyleOptionsMenu";
 import GlobalStyles from "../GlobalStyles";
 import Form from "../components/Form";
 import Sidebar from "../components/Sidebar";
+import { Route, Switch } from "react-router-dom";
+import ViewForm from "../components/ViewForm";
 
 export default function App () {
   return (
     <>
       <GlobalStyles />
-      <StyleOptionsMenu />
-      <Sidebar />
-      <Form />
+      <Switch>
+        <Route exact path='/'>
+          <Sidebar />
+          <Form />
+          <StyleOptionsMenu />
+        </Route>
+        <Route exact path='/:id'>
+          <ViewForm />
+        </Route>
+      </Switch>
     </>
   );
 }
